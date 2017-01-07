735e906763c3c277c5a366add20d44c4c1a069f6
Fred Bradley <hello@fredbradley.co.uk>
2017-01-07 10:07:40 +0000

    Change title to Marketing rather than news submissions

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Web News Submissions - @yield('title')</title>
+    <title>Cranleigh Marketing - @yield('title')</title>
 	
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
@@ -47,7 +47,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    News Submissions
+                    Cranleigh Marketing
                 </a>
             </div>
 
