3520e3ee7e7d17c13d09b5b1fd0a0932d6b1b8dd
Fred Bradley <hello@fredbradley.co.uk>
2017-03-10 11:46:46 +0000

    Limit login to only staff

--- config/adldap_auth.php
+++ config/adldap_auth.php
@@ -2,7 +2,9 @@
 return [
     'connection'            => env('ADLDAP_CONNECTION', 'default'),
     'username_attribute'    => ['email' => 'samaccountname'],
-    'limitation_filter'     => env('ADLDAP_LIMITATION_FILTER', '(CN=*)'),
+    //'limitation_filter'     => env('ADLDAP_LIMITATION_FILTER', '(CN=*)'),
+	'limitation_filter' => env('ADLDAP_LIMITATION_FILTER', '(|(memberof=CN=AllTeachingStaff,OU=Groups,DC=cranleigh,DC=org)(memberof=CN=Sec_Staff,OU=Groups,OU=Prep School Users,DC=cranleigh,DC=org))'),
+
     'login_fallback'        => env('ADLDAP_LOGIN_FALLBACK', false), // if this is set to true the limitation filter set above will not work if a user already exists in our users table
     'password_key'          => env('ADLDAP_PASSWORD_KEY', 'password'),
     'password_sync'         => env('ADLDAP_PASSWORD_SYNC', false),
